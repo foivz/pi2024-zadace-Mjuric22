0d6aaa1faddc3ba49952f2b628e6234c62c89dc7
Matej Jurić <mjuric22@foi.hr>
2024-06-12 20:12:58 +0200

    Napravljena forma unosa jela

---
+++ Software/SZUUP/SZUUP/FrmUnos.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SZUUP
+{
+    partial class FrmUnos
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.ComboBox cmbKategorija;
+        private System.Windows.Forms.TextBox txtCijena;
+        private System.Windows.Forms.TextBox txtDetalji;
+        private System.Windows.Forms.Button btnSpremi;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Button btnUredi;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.cmbKategorija = new System.Windows.Forms.ComboBox();
+            this.txtCijena = new System.Windows.Forms.TextBox();
+            this.txtDetalji = new System.Windows.Forms.TextBox();
+            this.btnSpremi = new System.Windows.Forms.Button();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.btnUredi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(107, 51);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(230, 26);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(120, 98);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(217, 26);
+            this.txtIme.TabIndex = 1;
+            // 
+            // cmbKategorija
+            // 
+            this.cmbKategorija.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbKategorija.FormattingEnabled = true;
+            this.cmbKategorija.Items.AddRange(new object[] {
+            "Juha",
+            "Glavno jelo",
+            "Predjelo",
+            "Desert",
+            "Voće"});
+            this.cmbKategorija.Location = new System.Drawing.Point(164, 144);
+            this.cmbKategorija.Name = "cmbKategorija";
+            this.cmbKategorija.Size = new System.Drawing.Size(173, 28);
+            this.cmbKategorija.TabIndex = 2;
+            this.cmbKategorija.SelectedIndexChanged += new System.EventHandler(this.cmbKategorija_SelectedIndexChanged);
+            // 
+            // txtCijena
+            // 
+            this.txtCijena.Location = new System.Drawing.Point(137, 191);
+            this.txtCijena.Name = "txtCijena";
+            this.txtCijena.Size = new System.Drawing.Size(200, 26);
+            this.txtCijena.TabIndex = 3;
+            // 
+            // txtDetalji
+            // 
+            this.txtDetalji.Location = new System.Drawing.Point(150, 230);
+            this.txtDetalji.Multiline = true;
+            this.txtDetalji.Name = "txtDetalji";
+            this.txtDetalji.Size = new System.Drawing.Size(187, 125);
+            this.txtDetalji.TabIndex = 4;
+            this.txtDetalji.TextChanged += new System.EventHandler(this.txtDetalji_TextChanged);
+            // 
+            // btnSpremi
+            // 
+            this.btnSpremi.Location = new System.Drawing.Point(187, 391);
+            this.btnSpremi.Name = "btnSpremi";
+            this.btnSpremi.Size = new System.Drawing.Size(81, 34);
+            this.btnSpremi.TabIndex = 5;
+            this.btnSpremi.Text = "Spremi";
+            this.btnSpremi.UseVisualStyleBackColor = true;
+            this.btnSpremi.Click += new System.EventHandler(this.btnSpremi_Click);
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(414, 391);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(86, 34);
+            this.btnOdustani.TabIndex = 6;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            this.btnOdustani.Click += new System.EventHandler(this.btnOdustani_Click);
+            // 
+            // btnUredi
+            // 
+            this.btnUredi.Location = new System.Drawing.Point(299, 391);
+            this.btnUredi.Name = "btnUredi";
+            this.btnUredi.Size = new System.Drawing.Size(82, 34);
+            this.btnUredi.TabIndex = 7;
+            this.btnUredi.Text = "Uredi";
+            this.btnUredi.UseVisualStyleBackColor = true;
+            this.btnUredi.Click += new System.EventHandler(this.btnUredi_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Id jela:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ime jela:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Kategorija jela:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Cijena jela:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Detalji o jelu:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(506, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Dostupna jela: Bečki odrezak, pileći file, bistra juha, kolač od čokolade..";
+            // 
+            // FrmUnos
+            // 
+            this.ClientSize = new System.Drawing.Size(542, 449);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUredi);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnSpremi);
+            this.Controls.Add(this.txtDetalji);
+            this.Controls.Add(this.txtCijena);
+            this.Controls.Add(this.cmbKategorija);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.txtID);
+            this.Name = "FrmUnos";
+            this.Text = "Unos jela";
+            this.Load += new System.EventHandler(this.FrmUnos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ Software/SZUUP/SZUUP/FrmUnos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SZUUP
+{
+    public partial class FrmUnos : Form
+    {
+        public FrmUnos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpremi_Click(object sender, EventArgs e)
+        {
+            // Save button click event handler
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+            // Cancel button click event handler
+        }
+
+        private void btnUredi_Click(object sender, EventArgs e)
+        {
+            // Edit button click event handler
+        }
+
+        private void FrmUnos_Load(object sender, EventArgs e)
+        {
+            // Form load event handler
+        }
+
+        private void cmbKategorija_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDetalji_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
